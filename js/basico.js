bdcbcd256a4591bb5ae048305170a0c85a0badfa
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-19 08:54:32 -0300

    variaveis let

--- js/basico.js
+++ js/basico.js
@@ -1,1 +1,18 @@
-console.log("aprendendo javascript")
+console.log("Comando para exibir dados no console")
+//alert("fazendo um alerta") // Só funciona na web
+
+console.log("Aprendendo sobre variaveis com javascript")
+
+let nome;
+
+console.log(nome)
+
+console.log("Variaveis que não foram iniciadas recebem undefined")
+
+nome = null
+
+console.log(nome)
+
+nome = 10
+
+console.log(nome)
