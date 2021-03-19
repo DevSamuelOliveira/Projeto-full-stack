2c6a033c13bb6e0f2a5bed0816482b34d73b5878
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-19 14:34:35 -0300

    alert, confirm e prompt

---
+++ js/alert_confirm_prompt.js
@@ -0,0 +1,8 @@
+alert("Alert é um método do objeto window que exibe uma menssagem no browser.\n\n"
++ "Observações devem ser feitas o alert segura o browser até o usuário clicar em ok e é retornado o valor undefined.")
+
+confirm("Confirm é semelhante a alert também vem do objeto window e exibe uma menssagem com 2 opções ok e cancelar.\n"
++ "se o usuário clicar em ok retorna true se clicar em cancelar retorna false.")
+
+prompt("Digite seu nome: ") //Quando clicado em ok retorna "" se nada for digitado ou o que for digitado no campo do input
+//se clicado em cancelar retorna null
