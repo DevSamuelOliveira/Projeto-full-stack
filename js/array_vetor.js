09fa76306f246de1881e9fcd0df252fa7d014d62
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-21 00:22:30 -0300

    atualização

--- js/array_vetor.js
+++ js/array_vetor.js
@@ -3,7 +3,3 @@ const shell = require('shelljs'); //npm install shelljs
 let nomes = ['samuel', 'amanda', 'natalia']
 
 console.log(nomes[1]);
-
-shell.exec('git add .')
-shell.exec('git commit -m "modulo shelljs instalado com npm e criado arquivo para trabalhar com array/vetores"')
-shell.exec('git push')
