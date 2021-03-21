6e642a1b32c2e2925245d2d8eb95be1a3f7fe25f
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-21 07:57:35 -0300

    funções basico

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+function exibir(nome = "Samuel"){
+  return "Bom dia " + nome
+}
+
+let exibir2 = function (nome = "Samuel") {
+  return "Bom dia " + nome
+}
+
+let exibir3 = (nome = "Samuel") => {
+  return "Bom dia " + nome
+}
+
+let exibir4 = nome => nome
+
+let exibir5 = (nome = "Samuel") => "Bom dia " + nome
+
+
