8faff4eca82bcee7c448744009ae35a4d7a943d1
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-20 22:54:09 -0300

    Modulo math

---
+++ js/math.js
@@ -0,0 +1,17 @@
+let num = 10.49
+
+console.log(Math.floor(num)) //arredonda um número para baixo
+
+console.log(Math.ceil(num)) //arredonda um número para cima
+
+console.log(Math.round(num)) //arredonda um número
+
+console.log(Math.max(10, 30, 40, 23, 87)) // retorna o maior número de uma sequência
+
+console.log(Math.min(10, 30, 40, 23, 87)) // retorna o menor número de uma sequência
+
+console.log(Math.random()) // gera um número aleatorio entre 0 e 1 mas não gera o 1
+
+//console.log(Math.random() * (start - end) + end) // gera um número aleatorio entre start - end
+
+console.log(Math.PI) // retorna o valor de PI
