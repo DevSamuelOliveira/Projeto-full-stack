238bb41389d5112201a4cf49953c1cc4c838f928
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-22 00:47:43 -0300

    entendendo a diferença de == e ===

---
+++ js/operadores_comparacao.js
@@ -0,0 +1,8 @@
+let valor1 = 10
+let valor2 = '10'
+
+//== compara apenas o valor independente do tipo
+//=== compara o valor e o tipo
+
+console.log(valor1 == valor2)
+console.log(valor1 === valor2)
