b2f5e227c406e85660792bd191edddac21d5f32e
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-25 23:01:04 -0300

    Estudando short-circuit(curto circuito)

---
+++ js/short-circuit.js
@@ -0,0 +1,12 @@
+//valores que retornam false
+
+//false
+//0
+//string vazia = "", '', ``
+//null / undefined
+//NaN
+
+//Qualquer coisa diferente disso retorna true
+
+console.log("samuel" && '' && "maria" || "Samuel") //Chega os valores se não tiver nenhum false retorna o ultimo valor checado, senão retorna o valor false
+//se tiver || (ou) retorna o primeiro valor verdadeiro
